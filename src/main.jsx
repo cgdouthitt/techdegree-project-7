c7f014bc50e4e89552e8c47cc59c62495e76d48f
cgdouthitt <46352253+cgdouthitt@users.noreply.github.com>
2025-01-13 11:06:35 -0600

    Adding strict mode back in

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,9 @@ import '../css/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  // <StrictMode>
-  //   <BrowserRouter>
-  //     <App />
-  //   </BrowserRouter>
-  // </StrictMode>,
+  </StrictMode>,
 )
